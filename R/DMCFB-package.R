3e11f27b9a960fbf97e7f1db21c5eb18f3238ada
Farhad Shokoohi <shokoohi@icloud.com>
2024-01-26 08:13:39 -0800

    cccmit

--- R/DMCFB-package.R
+++ R/DMCFB-package.R
@@ -1,10 +1,11 @@
-#' @title Differentially Methylated cytosines using funtional Bayesian
+#' @title Differentially Methylated cytosines using functional Bayesian
 #' regression models
 #'
 #' @description DMCFB is a profiling tool for identifying differentially
 #' methylated cytosines using Functional Bayesian Model in bisulfite sequencing
 #' data.
 #' @aliases DMCFB
+#' @aliases DMCFB-package
 #' @section \code{DMCFB} methods:
 #' \code{\link{findDMCFB}},
 #' \code{\link{plotDMCFB}},
@@ -13,6 +14,5 @@
 #' @section \code{BSDMC} objects:
 #' \code{\link{BSDMC-class}}
 #'
-#' @docType package
 #' @name DMCFB-package
 NULL
